235e434d8ae69bf8ca440183221fb0cc3b550f45
unknown <ocelot269@gmail.com>
2019-04-07 16:22:38 +0200

    creado la clase nachoVidal y puede calentar la habitacion y creado el metodo speak de nacho

---
+++ furnaceDIP/src/main/java/org/lasencinas/furnacedip/clases/NachoVidal.java
@@ -0,0 +1,43 @@
+
+package org.lasencinas.furnacedip.clases;
+
+import org.lasencinas.furnacedip.interfaces.Heater;
+
+
+public class NachoVidal implements Heater{
+
+    @Override
+    public void engage(Habitacion temperatura) {
+        temperatura.setAumentarTemperatura(1);
+    }
+    
+    
+    @Override
+    public void disengage(Habitacion temperatura) {
+        temperatura.setAumentarTemperatura(-1);
+    }
+    
+    
+    public void speak(){
+            System.out.println("...........///_\\\\ \n" +
+"..........||.^|^\\ \n" +
+"..........))_-_/ (( \n" +
+"..........)'_/.\"._`) \n" +
+"......././._..../... \n" +
+".....././(_.x/._).| \n" +
+".........)\".|.\"(/./ \n" +
+".........'...'.. //./ \n" +
+"......../....`..././ \n" +
+".......|....__.. \n" +
+".......|.../..../ \n" +
+".......|..|...... \n" +
+".......|..|....... \n" +
+".......|..|........ \n" +
+".......|..|......... \n" +
+".......|..|.......... \n" +
+".......|..|........... \n" +
+"......./__..........|__ \n" +
+"......_/.|............|._");
+}
+    
+}
--- furnaceDIP/src/main/java/org/lasencinas/furnacedip/main/FurnaceApp.java
+++ furnaceDIP/src/main/java/org/lasencinas/furnacedip/main/FurnaceApp.java
@@ -5,7 +5,7 @@ import org.lasencinas.furnacedip.clases.Regulador;
 import org.lasencinas.furnacedip.clases.SensorRemoto;
 import org.lasencinas.furnacedip.interfaces.Heater;
 import org.lasencinas.furnacedip.interfaces.Thermometer;
-//import org.foobarspam.furnaceDIP.otherstuff.Jedi;
+import org.lasencinas.furnacedip.clases.NachoVidal;
 import org.lasencinas.furnacedip.clases.Estufa;
 import org.lasencinas.furnacedip.clases.Habitacion;
 
@@ -28,9 +28,9 @@ public class FurnaceApp {
         System.out.println( "Arrancando..." );
         regulator.regulate(thermometer, heater, minTemp, maxTemp, Habitacion.getLaHabitacion());
         
-//        Jedi yoda = new Jedi();
-//        System.out.println( "\nArrancando a Yoda: " );
-//        regulator.regulate(thermometer, yoda, minTemp, maxTemp, temperature);
-//        yoda.speak();
+        NachoVidal nachete = new NachoVidal();
+        System.out.println( "\nArrancando a nacho: " );
+        regulator.regulate(thermometer, nachete, minTemp, maxTemp, Habitacion.getLaHabitacion());
+        nachete.speak();
     }
 }
